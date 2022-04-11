d561002c6531d3fe160fd7eda0649b60a5dcd6d1
Logan Williams <logan@firefly.llc>
2022-04-11 10:33:50 -0700

    Refactored index to split a Hero component from it.

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,43 @@
+<!-- Hero.svelte -->
+<script lang="ts">
+  import {tel} from './store'
+</script>
+
+<div class="relative">
+  <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+      <div class="absolute inset-0">
+        <video autoplay loop width="2235.73" height="1257.6" class="h-full w-full object-cover">
+          <source src="video/excavator.mp4" type="video/mp4" />
+          <track kind="captions" />
+        </video>
+        <!-- <div
+          class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"
+        /> -->
+      </div>
+      <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+        <img src="/FtS_Logo.svg" class="w-40 md:w-60 lg:w-80 mx-auto" alt="FtS Excavation logo" />
+        <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">
+          Proudly serving the South Puget Sound.
+        </p>
+        <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+          <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+            <a
+              href="/quote"
+              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-teal-700 bg-white hover:bg-indigo-50 sm:px-8"
+            >
+              Get A Quote
+            </a>
+            <a
+              href={$tel.href}
+              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-teal-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
+            >
+              Call Us
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,9 @@
 <!-- Hero section -->
-<div class="relative">
-  <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
-  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
-      <div class="absolute inset-0">
-        <video autoplay loop width="2235.73" height="1257.6" class="h-full w-full object-cover">
-          <source src="/video/excavator.mp4" type="video/mp4" />
-        </video>
-        <!-- <div
-          class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"
-        /> -->
-      </div>
-      <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-        <img src="/FtS_Logo.svg" class="w-40 md:w-60 lg:w-80 mx-auto" />
-        <!-- <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-          <span class="block text-white">Take control of your</span>
-          <span class="block text-indigo-200">customer support</span>
-        </h1> -->
-        <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">
-          Proudly serving the South Puget Sound.
-        </p>
-        <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-          <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-            <a
-              href="#"
-              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
-            >
-              Get A Quote
-            </a>
-            <a
-              href="#"
-              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
-            >
-              Call Us
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+<script lang="ts">
+  import Hero from '$lib/Hero.svelte'
+</script>
 
+<Hero />
 <!-- Logo Cloud -->
 <div class="bg-gray-100">
   <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
