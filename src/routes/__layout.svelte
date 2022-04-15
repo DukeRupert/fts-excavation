5b4af7fe443a8df2ff1ef434e3349a158255b5a9
Logan Williams <logan@firefly.llc>
2022-04-15 06:46:15 -0700

    Added G Analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,10 @@
   import '../app.css'
   import Header from '$lib/Header.svelte'
   import Footer from '$lib/Footer.svelte'
+  import {GoogleAnalytics} from '@beyonk/svelte-google-analytics'
 </script>
 
+<GoogleAnalytics properties={['G-RFVL3X4Y2Q']} />
 <Header />
 <div class="bg-white">
   <main>
