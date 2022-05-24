4323dcba55c9bb88001413d0ebb49975f1b9aa8b
Logan Williams <logan@firefly.llc>
2022-05-24 13:23:53 -0600

    Added youtube channel link to footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -18,7 +18,7 @@
         href="https://www.facebook.com/FtS-Excavation-107417208285323"
         class="text-gray-400 hover:text-gray-500"
       >
-        <span class="sr-only">Facebook</span>
+        <span class="sr-only">Facebook Page</span>
         <svg class="h-10 w-10" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
           <path
             fill-rule="evenodd"
@@ -26,6 +26,26 @@
             clip-rule="evenodd"
           />
         </svg>
+      </a>
+
+      <a
+        href="https://www.youtube.com/channel/UC_LbjVWjiiFg3h9lB6rcvGQ"
+        class="text-gray-400 hover:text-gray-500"
+      >
+        <span class="sr-only">Youtube Channel</span>
+        <svg
+          class="h-10 w-10"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path
+            d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"
+          /> <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" /></svg
+        >
       </a>
     </div>
     <p class="mt-8 text-center text-base text-gray-400">
