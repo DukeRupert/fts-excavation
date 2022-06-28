35dd18a8b7337741b485b08d407a6c2ffd137cec
Logan Williams <logan@firefly.llc>
2022-06-28 13:16:41 -0600

    Added phone number to Footer. Edited styles

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,6 @@
 <!-- footer.svelte -->
 <script lang="ts">
-  import {links} from './store'
+  import {tel, links} from './store'
   import ChamberOfCommerce from './ChamberOfCommerce.svelte'
 </script>
 
@@ -16,7 +16,7 @@
     <div class="mt-8 flex justify-center space-x-6">
       <a
         href="https://www.facebook.com/FtS-Excavation-107417208285323"
-        class="text-gray-400 hover:text-gray-500"
+        class="text-actionorange-500 hover:text-actionorange-300"
       >
         <span class="sr-only">Facebook Page</span>
         <svg class="h-10 w-10" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -30,7 +30,7 @@
 
       <a
         href="https://www.youtube.com/channel/UC_LbjVWjiiFg3h9lB6rcvGQ"
-        class="text-gray-400 hover:text-gray-500"
+        class="text-actionorange-500 hover:text-actionorange-300"
       >
         <span class="sr-only">Youtube Channel</span>
         <svg
@@ -50,8 +50,24 @@
     </div>
     <a
       href="https://www.google.com/maps/place/FtS-Excavation,+LLC/@47.1761259,-122.2630845,10z/data=!4m5!3m4!1s0x0:0xb896498a4891ace9!8m2!3d47.1761259!4d-122.2630845"
-      class="mt-8 block text-center text-base text-gray-400">12714 151st St E Puyallup, WA 98374</a
+      class="mt-8 block text-center text-base text-evergreen-500"
+      >12714 151st St E Puyallup, WA 98374</a
     >
+    <a
+      href={$tel.href}
+      class="mt-8 flex mx-auto items-center justify-center text-center text-base text-evergreen-500"
+    >
+      <svg class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+        />
+      </svg>
+
+      {$tel.pretty}
+    </a>
     <p class="mt-8 text-center text-base text-gray-400">
       &copy; 2021 FTS Excavation, LLC. All rights reserved.
     </p>
